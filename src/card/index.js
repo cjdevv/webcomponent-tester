eb47c1c04962dbec6367e0c516bf9a4945e3f51e
carlos <cjdevv@gmail.com>
2019-02-13 21:26:24 -0500

    make card visible

--- src/card/index.js
+++ src/card/index.js
@@ -1,7 +1,22 @@
+const template = document.createElement('template');
+template.innerHTML = `
+    <style>
+      :host {
+        display: inline-block;
+        border: black solid;
+        width: 24px;
+        height: 24px;
+      }
+    </style>
+  `;
+
 class Card extends HTMLElement {
     constructor() {
         super();
         console.log('its a me, a card');
+
+        this.attachShadow({mode: 'open'});
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
 }
 
